7094a9cb351988b03b2d2509d8c39d17bc072d2c
王逍遥 <wangxiaoyao77@gmail.com>
2019-08-28 14:24:54 +0800

    比较两个字符串是否相等

---
+++ 19-8-28compare-string/main.c
@@ -0,0 +1,39 @@
+//
+//  main.c
+//  test
+//
+//  Created by 王逍遥 on 2019/8/24.
+//  Copyright © 2019 王逍遥. All rights reserved.
+//
+
+#include <stdio.h>
+
+// 比较两个字符串是否相同
+void cmpXiao(char *str1,char *str2){
+    unsigned long len1 = strlen(str1);
+    unsigned long len2 = strlen(str2);
+    if (str1[3]==str2[3]) {
+        printf("！相等");
+        printf("证明对于字符串末端存在二进制0，且在循环中也有占位，所以index为4的原因");
+    }
+    if (len1!=len2) {
+        printf("这两个字符串不相等\n");
+    }else{
+        int index=0;
+        while (str1[index] ==str2[index]) {
+            index++;
+        }
+        if (index-1 == len1) {
+            printf("这两个字符串相等\n");
+        }else{
+            printf("这两个字符串不相等\n");
+        }
+    }
+}
+int main(int argc, const char * argv[]) {
+    
+    char str1[] = "abcd";
+    char str2[] = "abc ";
+    cmpXiao(str1,str2);
+    return 0;
+}
