47ed3dccb0975c8aa9291aa2f946b707025849be
王逍遥 <wangxiaoyao77@gmail.com>
2019-08-29 16:59:26 +0800

    指针数组

---
+++ 19-8-29pointer-array/main.c
@@ -0,0 +1,45 @@
+//
+//  main.c
+//  test
+//
+//  Created by 王逍遥 on 2019/8/24.
+//  Copyright © 2019 王逍遥. All rights reserved.
+//
+
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+
+char *sortXiao(char *str[],int num){
+    char temp[100];
+    int index = 0;
+    for (int i=0; i<num; i++) {
+        for (int j=0; j< strlen(*(str+i)); j++) {
+//            strcat(temp,*(str[i]+j));
+            temp[index] = *(str[i]+j);
+            index++;
+        }
+    };
+    char *p = temp;
+//    printf("%s\n",p9);
+    unsigned long len = strlen(temp);
+    for (int i =0; i<len; i++) {
+        for (int j=i+1;j<len;j++ ) {
+            if (temp[i]>temp[j]) {
+                char tempChr = temp[i];
+                temp[i] = temp[j];
+                temp[j] = tempChr;
+            }
+        }
+    }
+    return p;
+}
+
+// 字符串数组排序A-Z sortXiao
+int main(int argc, const char * argv[]) {
+    char *str[]={"hello","world","linger"};
+    int num = 3;
+    char *p = sortXiao(str,num);
+    printf("%s\n",p);
+    return 0;
+}
