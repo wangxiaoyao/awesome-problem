d83f413e3a1e172370a46195a9088216c110b760
王逍遥 <wangxiaoyao77@gmail.com>
2019-12-29 20:02:07 +0800

    简单修改

--- 19-8-24flowers-problem/main.c
+++ 19-8-24flowers-problem/main.c
@@ -10,7 +10,7 @@
 
 int main(int argc, const char * argv[]) {
     int a ,b,c;
-    for (a=0; a<10; a++) {
+    for (a=1; a<10; a++) {
         for (b=0; b<10; b++) {
             for (c=0; c<10; c++) {
                 if (a*a*a+b*b*b+c*c*c==a*100+b*10+c) {
